b25f8cf808ee5de53b187892a943cef00ccab9d9
amarjeet2611 <123499426+amarjeet2611@users.noreply.github.com>
2023-10-15 22:21:53 +0530

    Divide_Two_Integers.cpp

--- Divide_Two_Integers.cpp
+++ Divide_Two_Integers.cpp
@@ -3,6 +3,5 @@ public:
     int divide(int dividend, int divisor) {
         if(dividend == -2147483648 && divisor == -1) return 2147483647;
         int div = dividend/divisor;
-        return div;
-    }
+        return div; }
 };
